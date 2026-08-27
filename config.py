a86111c2214249f72b112a4ae02d2ca980cd83e4
Shobhit Agrawal <shobhitag001@gmail.com>
2026-08-27 21:04:47 +0530

    Finalize Ghost Merchant risk investigation platform

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-NEO4J_URI = "bolt://localhost:7687"
+import os
+from dotenv import load_dotenv
 
-NEO4J_USERNAME = "neo4j"
+load_dotenv()
 
-NEO4J_PASSWORD = "shobhitag"
+NEO4J_URI = os.getenv("NEO4J_URI")
+NEO4J_USERNAME = os.getenv("NEO4J_USERNAME")
+NEO4J_PASSWORD = os.getenv("NEO4J_PASSWORD")
